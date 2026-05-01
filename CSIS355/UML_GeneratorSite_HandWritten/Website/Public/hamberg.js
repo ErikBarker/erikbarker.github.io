c3df3f03cdc7f6c05349ff7983feb88cb9b72bb7
Erik Barker <112636699+ErikBarker@users.noreply.github.com>
2026-05-01 00:40:13 -0400

    Delete CSIS355/UML_GeneratorSite_HandWritten/Website/Public directory

--- CSIS355/UML_GeneratorSite_HandWritten/Website/Public/hamberg.js
+++
@@ -1,7 +0,0 @@
-const hambergerbutton = document.getElementById("hamburger");
-const hambergermenu = document.getElementById("hamburgerMenu");
-
-hambergerbutton.addEventListener("click",()=>{
-    console.log("click")
-    hambergermenu.classList.toggle("hidden")
-});
