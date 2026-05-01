98ca737adc2460b7f287cb547b7133d962717d55
Erik Barker <112636699+ErikBarker@users.noreply.github.com>
2026-05-01 00:12:28 -0400

    Update server.js

--- CSIS355/UML_GeneratorSite_HandWritten/Website/backend/server.js
+++ CSIS355/UML_GeneratorSite_HandWritten/Website/backend/server.js
@@ -125,6 +125,10 @@ app.use(errorHandler);
 //server logic for routes
 app.use(express.static(path.join(__dirname, '../public')));
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/umlGenVertion', require('./routes/umlGenVertionRoutes'));
 
